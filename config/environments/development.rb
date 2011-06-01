d956d820a15cc111884e973a650b6cc0baad244d
usabi <infousabi@gmail.com>
2011-06-01 10:44:35 +0200

    creamos el formulario

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Usabi::Application.configure do
+FisioApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Usabi::Application.configure do
+FisioApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # The production environment is meant for finished, "live" apps.
