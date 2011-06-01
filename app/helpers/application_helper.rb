440384b8e3008bf227e9f82a52619d63739f0f1e
usabi <infousabi@gmail.com>
2011-06-01 18:04:29 +0200

    subimos las imagenes de las webs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,20 @@ module ApplicationHelper
 	
 	def title
 	end
+	def plantilla1
+	  image_tag("plantilla1.png", :alt => "Web Básica: desde 170€", :class => "ejemploimg")
+	end
+	def plantilla2
+    image_tag("plantilla2.png", :alt => "Web Estándar: desde 300€", :class => "ejemploimg")
+  end
+  def plantilla3
+    image_tag("plantilla3.png", :alt => "Web Empresa: desde 700€", :class => "ejemploimg")
+  end
+  def plantilla4
+    image_tag("plantilla4.png", :alt => "Tienda online: desde 600€", :class => "ejemploimg")
+  end
+  def plantilla5
+    image_tag("plantilla5.png", :alt => "Web Personalizada", :class => "ejemploimg")
+  end	
+	
 end
